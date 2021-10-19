3d48bb5c3054cccbcb1a3b1033c3e90cf881cb6f
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-19 18:48:48 -0400

    skeleton of exercise 42

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+public class Solution42 {
+    public static void main(String[] args){
+
+        /*
+        read in the data from the input file
+        process the data in between each comma as a record
+        sort the data into a table
+        output the table
+         */
+
+    }
+}
