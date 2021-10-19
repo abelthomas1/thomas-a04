fef401106009459621dc7d6ef1219185bfc8603c
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-19 18:09:43 -0400

    skeleton of exercise 41

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+public class Solution41 {
+    public static void main(String[] args) {
+
+        /*
+        read data from the text file given
+        determine the alphabetic order of the names within the file
+        sort the names according to this order
+        output the sorted list of names
+         */
+
+    }
+}
