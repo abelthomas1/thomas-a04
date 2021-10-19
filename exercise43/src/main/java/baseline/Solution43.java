bdfba04acfc9013400eabdf6a59c06fd2811f011
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-19 19:42:13 -0400

    skeleton of exercise 43

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+public class Solution43 {
+    public static void main(String[] args) {
+
+        /*
+        Prompt for the name of the site.
+        Prompt for the author of the site.
+        Ask if the user wants a folder for JavaScript files.
+        Ask if the user wants a folder for CSS files.
+        Generate an index.html file that contains the name of the site inside the <title> tag and the author in a <meta> tag.
+         */
+
+    }
+}
