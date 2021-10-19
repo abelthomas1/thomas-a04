1e0c0b1ba8badee8a70a380864fcc90a8deb5d81
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-19 18:38:47 -0400

    final exercise 41

--- exercise41/src/main/java/baseline/Solution41.java
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -5,8 +5,11 @@
 
 package baseline;
 
+import java.io.*;
+
+import java.util.*;
+
 public class Solution41 {
-    public static void main(String[] args) {
 
         /*
         read data from the text file given
@@ -15,5 +18,39 @@ public class Solution41 {
         output the sorted list of names
          */
 
+    public static void readNames(Scanner input, ArrayList<String> names) {
+        while (input.hasNextLine()) {
+            names.add(input.nextLine());
+        }
+    }
+
+    public static void outputNames(ArrayList<String> names) throws IOException {
+        FileWriter output = new FileWriter("exercise41_output.txt");
+        output.write("Total of " + (names.size() - 1) + " names\n");
+        output.write("--------------------\n");
+
+        for (String i : names) {
+            output.write(i + "\n");
+        }
+        output.close();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner inputFile = new Scanner(new File("java/exercise41_input.txt"));
+        ArrayList<String> names = new ArrayList<String>();
+
+        readNames(inputFile, names);
+
+        Collections.sort(names, String.CASE_INSENSITIVE_ORDER);
+
+        inputFile.close();
+
+        try {
+            outputNames(names);
+        }
+
+        catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
