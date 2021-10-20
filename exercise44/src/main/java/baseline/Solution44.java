d5286a072adfa11b3428ee10d889b88d7e459876
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-20 03:18:37 -0400

    skeleton of exercise 44

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+public class Solution44 {
+    public static void main(String[] args){
+
+        /*
+        prompt the user for a product name
+        parse the input file for the product given
+        if the product does not exist, try again
+        print the price and quantity of the item given
+         */
+
+    }
+}
