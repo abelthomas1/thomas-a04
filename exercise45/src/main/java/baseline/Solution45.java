221fbc65526d3f7ae64f266bbfe1ecb7208ce75d
Abel Thomas <90160007+abelthomas1@users.noreply.github.com>
2021-10-20 03:39:56 -0400

    skeleton of exercise 45

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Abel Thomas
+ */
+
+package baseline;
+
+public class Solution45 {
+    public static void main(String[] args){
+
+        /*
+        read input file given
+        replace all instances of utilize with use
+        output the modified file
+         */
+
+    }
+}
